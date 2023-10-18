48bc16c18c757833084d7a18ac001ba84aa7f645
JinSol <s9567695@gmail.com>
2023-10-18 12:20:10 +0900

    Update: 기능 수정
    - 난수 생성 부분 getRandomValues() 메서드로 수정

--- assets/js/lotto.js
+++ assets/js/lotto.js
@@ -1,6 +1,7 @@
 let arr = [];
 let suffleInterval = 0;
 let loopFlag = false;
+const intArray = new Uint8Array(1);
 
 $(document).ready(function() {
   initNumber();
@@ -22,9 +23,11 @@ $(document).ready(function() {
 
     var selectIntervalNumber = $('#selectInterval').val();
     var interval = setInterval(() => {
-      var randomNumber = Math.floor(Math.random() * 100);
-      while(randomNumber === 0 || arr.indexOf(randomNumber) == -1) {
-        randomNumber = Math.floor(Math.random() * 100);
+      window.crypto.getRandomValues(intArray);
+      var randomNumber = intArray[0] % 45 + 1;
+      while(arr.indexOf(randomNumber) == -1) {
+        window.crypto.getRandomValues(intArray);
+        randomNumber = intArray[0] % 45 + 1;
       }
       
       const index = arr.findIndex((e) => e === randomNumber);
@@ -59,10 +62,13 @@ $(document).ready(function() {
 
 function initNumber() {
   arr = [];
+
   for(let i = 0; i < 45; i++) {
-    let number = Math.floor(Math.random() * 100);
-    while(number == 0 || number > 45 || arr.indexOf(number) != -1) {
-      number = Math.floor(Math.random() * 100);
+    window.crypto.getRandomValues(intArray);
+    let number = intArray[0] % 45 + 1;
+    while(arr.indexOf(number) != -1) {
+      window.crypto.getRandomValues(intArray);
+      number = intArray[0] % 45 + 1;
     }
     arr.push(number);
   }
@@ -79,17 +85,21 @@ function startSuffle() {
 function suffle() {
   for(let i = 0; i < arr.length; i++) {
     // 배열에 존재하는 값이 나올 때까지 랜덤한 값 선택
-    let randomNumber1 = Math.floor(Math.random() * 100);
-    while(randomNumber1 === 0 || arr.indexOf(randomNumber1) == -1) {
-      randomNumber1 = Math.floor(Math.random() * 100);
+    window.crypto.getRandomValues(intArray);
+    let randomNumber1 = intArray[0] % 45 + 1;
+    while(arr.indexOf(randomNumber1) == -1) {
+      window.crypto.getRandomValues(intArray);
+      randomNumber1 = intArray[0] % 45 + 1;
     }
     // arr에서 랜덤한 값과 일치하는 요소의 index 반환. 섞을 대상 A
     const suffleIndexA = arr.findIndex((e) => e === randomNumber1);
   
     // 배열에 존재하는 값이 나올 때까지 랜덤한 값 선택
-    let randomNumber2 = Math.floor(Math.random() * 100);
-    while(randomNumber2 === 0 || arr.indexOf(randomNumber2) == -1) {
-      randomNumber2 = Math.floor(Math.random() * 100);
+    window.crypto.getRandomValues(intArray);
+    let randomNumber2 = intArray[0] % 45 + 1;
+    while(arr.indexOf(randomNumber2) == -1) {
+      window.crypto.getRandomValues(intArray);
+      randomNumber2 = intArray[0] % 45 + 1;
     }
     // arr에서 랜덤한 값과 일치하는 요소의 index 반환. 섞을 대상 B
     const suffleIndexB = arr.findIndex((e) => e === randomNumber2);
@@ -99,6 +109,21 @@ function suffle() {
     arr[suffleIndexA] = arr[suffleIndexB];
     arr[suffleIndexB] = temp;
   }
-  
-  startSuffle();
+}
+
+function selectNumbersAtOnce() {
+  const selectedNumber = [];
+  const array = new Uint8Array(1);
+
+  while(selectedNumber.length < 6) {
+    window.crypto.getRandomValues(array);
+    const randomNumber = array[0] % 45 + 1;
+    if(selectedNumber.indexOf(randomNumber) == -1) {
+      selectedNumber.push(randomNumber);
+    }
+  }
+
+  selectedNumber.sort((a, b) => a - b);
+
+  console.log(selectedNumber);
 }
