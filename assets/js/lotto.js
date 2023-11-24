32c218721468fd3c457aad8513ce1567efb64dd4
JinSol <s9567695@gmail.com>
2023-11-24 10:16:06 +0900

    Feat: Lotto Generator 기능 추가
    - 한번에 번호를 생성하는 기능 추가

--- assets/js/lotto.js
+++ assets/js/lotto.js
@@ -6,60 +6,97 @@ const intArray = new Uint8Array(1);
 $(document).ready(function() {
   initNumber();
   
+  // 번호생성 button 클릭
   $('#btn_select').on('click', function() {
-    if(loopFlag) {
-      return;
-    }
-
-    if(!$('#suffleInterval').val() || !$('#selectInterval').val()) {
-      alert('입력란은 필수값입니다.');
-      return;
-    }
-
-    loopFlag = true;
-    $('#btn_select').text('생성중...');
-    $('#suffleInterval, #selectInterval').prop('disabled', true);
-    startSuffle();
-
-    var selectIntervalNumber = $('#selectInterval').val();
-    var interval = setInterval(() => {
-      window.crypto.getRandomValues(intArray);
-      var randomNumber = intArray[0] % 45 + 1;
-      while(arr.indexOf(randomNumber) == -1) {
-        window.crypto.getRandomValues(intArray);
-        randomNumber = intArray[0] % 45 + 1;
+    if($('input:radio[name=generateType]:checked').val() === 'SEQ') {
+      if(loopFlag) {
+        return;
       }
-      
-      const index = arr.findIndex((e) => e === randomNumber);
-      arr.splice(index, 1);
-      
-      $('div.log').last().append(
-        '<span style="margin-right: 10px;">'+ randomNumber +'</span>'
-      );
-      
-      if($('span', $('div.log')).length % 6 == 0) {
-        initNumber();
+  
+      if(!$('#suffleInterval').val() || !$('#selectInterval').val()) {
+        alert('입력란은 필수값입니다.');
+        return;
+      }
+  
+      loopFlag = true;
+      $('#btn_select').text('생성중...');
+      $('#suffleInterval, #selectInterval').prop('disabled', true);
+      startSuffle();
+  
+      var selectIntervalNumber = $('#selectInterval').val();
+      var interval = setInterval(() => {
+        window.crypto.getRandomValues(intArray);
+        var randomNumber = intArray[0] % 45 + 1;
+        while(arr.indexOf(randomNumber) == -1) {
+          window.crypto.getRandomValues(intArray);
+          randomNumber = intArray[0] % 45 + 1;
+        }
         
-        $('div.container').append(
-            '<div class="row">'+
-            '<div class="col d-flex justify-content-center log"></div>'+
-            '</div>'
+        const index = arr.findIndex((e) => e === randomNumber);
+        arr.splice(index, 1);
+        
+        $('div.log').last().append(
+          '<span class="me-3">'+ randomNumber +'</span>'
         );
         
-        clearInterval(interval);
-        clearTimeout(suffleInterval);
-        $('#btn_select').text('번호생성');
-        $('#suffleInterval, #selectInterval').prop('disabled', false);
-        loopFlag = false;
-      }
-    }, selectIntervalNumber);
+        if($('span', $('div.log')).length % 6 == 0) {
+          initNumber();
+          
+          $('div.container').append(
+              '<div class="row">'+
+              '<div class="col d-flex justify-content-center log"></div>'+
+              '</div>'
+          );
+          
+          clearInterval(interval);
+          clearTimeout(suffleInterval);
+          $('#btn_select').text('번호생성');
+          $('#suffleInterval, #selectInterval').prop('disabled', false);
+          loopFlag = false;
+        }
+      }, selectIntervalNumber);
+    } else {
+      const selectedNumberList = selectNumbersAtOnce();
+      $.each(selectedNumberList, function(idx, obj) {
+        $('div.log').last().append(
+          '<span class="me-3">'+ obj +'</span>'
+        );
+      });
+      $('div.container').append(
+        '<div class="row">'+
+        '<div class="col d-flex justify-content-center log"></div>'+
+        '</div>'
+      );
+    }
   });
   
+  // number 이벤트
   $('#suffleInterval, #selectInterval').on('input', function() {
     $(this).val($(this).val().replace(/[^0-9]/g, ''));
   });
+  $('#suffleInterval, #selectInterval').on('blur', function() {
+    if($(this).val() < $(this).attr('min')) {
+      $(this).val($(this).attr('min'));
+    }
+  });
+
+  // radio 이벤트
+  $('input:radio[name=generateType]').on('change', function() {
+    if($(this).val() === 'SEQ') {
+      $('#parameterSeqType').show();
+    } else {
+      $('#parameterSeqType').hide();
+    }
+    $('div.row').each(function() {
+      if(!$(this).hasClass('mt-3')) {
+        $(this).remove();
+      }
+    });
+    $('span.me-3').remove();
+  });
 });
 
+// 배열에 숫자를 1-45로 초기화
 function initNumber() {
   arr = [];
 
@@ -74,6 +111,7 @@ function initNumber() {
   }
 }
 
+// suffleInterval 주기로 suffle() 메서드를 호출
 function startSuffle() {
   var intervalNumber = $('#suffleInterval').val();
   suffleInterval = setTimeout(() => {
@@ -81,7 +119,7 @@ function startSuffle() {
   }, intervalNumber);
 }
 
-// 숫자를 한번 섞음
+// 배열을 한번 섞음
 function suffle() {
   for(let i = 0; i < arr.length; i++) {
     // 배열에 존재하는 값이 나올 때까지 랜덤한 값 선택
@@ -111,6 +149,7 @@ function suffle() {
   }
 }
 
+// 1-45까지의 숫자 중 6개 선택
 function selectNumbersAtOnce() {
   const selectedNumber = [];
   const array = new Uint8Array(1);
@@ -125,5 +164,5 @@ function selectNumbersAtOnce() {
 
   selectedNumber.sort((a, b) => a - b);
 
-  console.log(selectedNumber);
+  return selectedNumber;
 }
